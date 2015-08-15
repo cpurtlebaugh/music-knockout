274e3f5506976811f1f3f802693ccf6215584f0e
Taylor Harwood <htaylor127@gmail.com>
2015-08-14 17:48:56 -0700

    changes...

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,9 @@
-var test = $.get(https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists).then(function(x){var test = $.get(https://api.spotify.com/v1/artists/3nFkdlSjzX9mRTtwJOzDYB/related-artists).then(function(x){
+var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){var test = $.get("https://api.spotify.com/v1/artists/3nFkdlSjzX9mRTtwJOzDYB/related-artists").then(function(x){
   x.artists.map(function(y){
-    $.get(https://api.spotify.com/v1/artists/ + y.id + /top-tracks?country=US).then(function(z){
+    $.get("https://api.spotify.com/v1/artists/" + y.id + "/top-tracks?country=US").then(function(z){
       console.log(z);
     });
   });
 });
   console.log(x.artists);
 });
-
