66f81c083e16b66c3d803feafcaa2f6218897124
Taylor Harwood <htaylor127@gmail.com>
2015-08-16 15:19:43 -0700

    added basic controllers and routes, not finished

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,48 @@
 var User = require('../models/User');
 
-module.exports = {
+function getAll(req, res, err) {
 
+}
+
+function newUser(req, res, err) {
+  response.render('../views/users/new');
+}
+
+function createUser(req, res, err) {
+  var user = new User(request.body.user);
+  User.save(function(error){
+    if (error) response.json({message: 'Could not create user b/c:' + error});
+  });
+  response.redirect('/');
+}
+
+function showUser(req, res, err) {
+  var user = User.findByid({_id: req.params.id}, function(error, user){
+    if(error) response.json({message: 'Could not find that user b/c:' + error});
+    response.render('/users/:id', {user: user});
+  });
+}
+
+function editUser(req, res, err) {
+
+}
+
+function updateUser(req, res, err) {
+
+}
+
+function removeUser(req, res, err) {
+
+}
+
+
+
+module.exports = {
+  getAll:     getAll,
+  newUser:    newUser,
+  createUser: createUser,
+  showUser:   showUser,
+  editUser:   editUser,
+  updateUser: updateUser,
+  removeUser: removeUser
 }
