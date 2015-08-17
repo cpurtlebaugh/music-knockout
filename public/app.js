3e0b0da03fd42297a523c26931657ec32e3a5dcb
Taylor Harwood <htaylor127@gmail.com>
2015-08-16 18:18:11 -0700

    making slight progress on ajax requests

--- public/app.js
+++
@@ -1,6 +0,0 @@
-$.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){
-  x.map(function(y){
-    console.log(y.name);
-  });
-});
-
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,12 +1,24 @@
 console.log('connected JS file');
 
+var genre = "";
+var songs = [];
+
 var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){
   x.artists.map(function(y){
     $.get("https://api.spotify.com/v1/artists/" + y.id + "/top-tracks?country=US").then(function(z){
-      console.log(z);
+      z.tracks.map(function(a){
+        songs.push(a.href);
+        console.log(songs);
+      });
+    }).then(function(){
+        console.log(songs[1]);
+        $('body').append('<iframe src="https://embed.spotify.com/?uri=spotify:track:"' +  songs[0] + ' ' + 'width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
     });
   });
 });
+
+console.log(songs);
+
 
 
 // $(document).ready(function(){
