21fa5b47ad6c59d16d27911d9d81b230e056672f
Taylor Harwood <htaylor127@gmail.com>
2015-08-16 17:03:34 -0700

    got the code working again, kind of

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,6 +9,6 @@ var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/rela
 });
 
 
-  $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
