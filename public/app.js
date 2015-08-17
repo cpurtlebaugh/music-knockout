7b52bb9decd8724f52189ff8584271541eab88b5
Taylor Harwood <htaylor127@gmail.com>
2015-08-16 17:22:17 -0700

    renamed main server.js file, worked on ajax stuff (still not working

--- public/app.js
+++ public/app.js
@@ -1,9 +1,6 @@
-var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){
-  x.artists.map(function(y){
-    console.log(y);
-    // $.get("https://api.spotify.com/v1/artists/" + y.id + "/top-tracks?country=US").then(function(z){
-    //   console.log(z);
-    // });
+$.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){
+  x.map(function(y){
+    console.log(y.name);
   });
 });
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,16 @@
-var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){var test = $.get("https://api.spotify.com/v1/artists/3nFkdlSjzX9mRTtwJOzDYB/related-artists").then(function(x){
-  x.artists.map(function(y){
-    $.get("https://api.spotify.com/v1/artists/" + y.id + "/top-tracks?country=US").then(function(z){
-      console.log(z);
-    });
-  });
-});
-  console.log(x.artists);
-});
+console.log('connected JS file');
+
+// var test = $.get("https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists").then(function(x){var test = $.get("https://api.spotify.com/v1/artists/3nFkdlSjzX9mRTtwJOzDYB/related-artists").then(function(x){
+//   x.artists.map(function(y){
+//     $.get("https://api.spotify.com/v1/artists/" + y.id + "/top-tracks?country=US").then(function(z){
+//       console.log(z);
+//     });
+//   });
+// });
+//   console.log(x.artists);
+// });
 
 
-$(document).ready(function(){
-  $('.parallax').parallax();
-});
+// $(document).ready(function(){
+//   $('.parallax').parallax();
+// });
